8650e24d475497cde3463001fa4e448be1cea893
Mikołaj <mikolaj.morzy@gmail.com>
2014-09-09 03:12:42 +0200

    Backward and forward similarity plots

--- influence.mining.R
+++ influence.mining.R
@@ -2,18 +2,6 @@
 # Research on the textual influence between groups of users of the popular link aggregator Wykop.pl
 #
 
-# read input data
-data <- read.csv(file = "wykop_comments_1-7days-no_noise.csv", header = TRUE, sep = ";")
-attach(data)
-
-# compute forward similarities across categories
-aggregated.columns <- cbind(avg_sim_after1,avg_sim_after2,avg_sim_after3,avg_sim_after4,avg_sim_after5,avg_sim_after6,avg_sim_after7)
-similarity.forward.by.category <- aggregate(aggregated.columns~category, data, mean)
-
-# compute backward similarities across categories
-aggregated.columns <- cbind(avg_sim_before1,avg_sim_before2,avg_sim_before3,avg_sim_before4,avg_sim_before5,avg_sim_before6,avg_sim_before7)
-similarity.backward.by.category <- aggregate(aggregated.columns~category, data, mean)
-
 # read data on random similarity between comments
 # the file contains the similarity between a few selected comments and all other comments in the dataset
 # this allows to compute the average expected similarity between any pair of comments
@@ -54,9 +42,70 @@ aggregated.random.comments.stats
 library(ggplot2)
 
 png("ecdf.comments.png")
-plot <- ggplot(random.comments.data, aes(x = sim)) + stat_ecdf(aes(colour = "red")) + ggtitle("Empirical cumulative density function") + labs(x = "similarity") + labs(y = "probability") + theme(legend.position = "none")
+plot <- ggplot(random.comments.data, aes(x = sim)) + stat_ecdf(aes(colour = "red")) + 
+  ggtitle("Empirical cumulative density function") + 
+  labs(x = "similarity") + 
+  labs(y = "probability") + 
+  theme(legend.position = "none")
 plot
 dev.off()
 
+################################ data analysis #######################################
 
+# read input data
+data <- read.csv(file = "wykop_comments_1-7days-no_noise.csv", header = TRUE, sep = ";")
+attach(data)
+
+# compute forward similarities across categories
+aggregated.columns <- cbind(avg_sim_after1,avg_sim_after2,avg_sim_after3,avg_sim_after4,avg_sim_after5,avg_sim_after6,avg_sim_after7)
+similarity.forward.by.category <- aggregate(aggregated.columns~category, data, mean)
+
+# compute backward similarities across categories
+aggregated.columns <- cbind(avg_sim_before1,avg_sim_before2,avg_sim_before3,avg_sim_before4,avg_sim_before5,avg_sim_before6,avg_sim_before7)
+similarity.backward.by.category <- aggregate(aggregated.columns~category, data, mean)
+
+# how many comments are there on average in each category
+# the plot shows the histogram and the density of the number of comments by categories,
+# with the vertical red line showing the mean number of comments (16144)
+count.comments.by.category <- aggregate(comment_count~category, data, sum)
+
+png("count.comments.png")
+plot <- ggplot(count.comments.by.category, aes(x = comment_count)) +
+  geom_density(color = "black") +
+  geom_histogram(aes(y = ..density..), alpha = 0.3, fill = "blue", position = "identity") +
+  ggtitle("distribution of comments across categories") +
+  labs(x = "number of comments") +
+  labs(y = "density") +
+  geom_vline(xintercept = mean(count.comments.by.category$comment_count), colour = "red")
+plot
+dev.off()
+
+# how does the similarity change between days from the publication date
+# the question is: across all categories, are comments becoming less similar as the time goes by?
+
+# backward similarity
+backward.between.day.similarity <- c(1:6)
+for (i in backward.between.day.similarity) {
+  backward.between.day.similarity[i] <- mean(similarity.backward.by.category[,i+1]) - mean(similarity.backward.by.category[,i+2]) 
+}
+
+#forward similarity
+forward.between.day.similarity <- c(1:6)
+for (i in forward.between.day.similarity) {
+  forward.between.day.similarity[i] <- mean(similarity.forward.by.category[,i+1]) - mean(similarity.forward.by.category[,i+2]) 
+}
+
+df <- data.frame(cbind(id = rep(c(1:6),2), melt(list(bbds = backward.between.day.similarity, fbds = forward.between.day.similarity))))
+
+png("back.forw.similarity.change.png")
+plot <- ggplot(data = df, aes(x = id, y = value, colour = L1)) + 
+  geom_line() +
+  scale_x_continuous(breaks = 1:6) +
+  scale_color_discrete(breaks = c("bbds", "fbds"), labels = c("backward", "forward")) +
+  guides(color = guide_legend(title = NULL)) +
+  ggtitle("average similarity between days") + 
+  labs(x = "day") +
+  labs(y = "similarity difference")
+plot
+dev.off()
 
